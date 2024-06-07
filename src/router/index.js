a7955ac5418cefe27105172cc5a3fd443cab39ee
Foz1ljon <faxriddinov0620@gmail.com>
2024-06-07 22:07:37 +0500

    v1

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 
 const routes = [
   {
@@ -35,6 +35,6 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
