ede4a22b88a7027638ce7ab1963b5ce61e4bb491
Foz1ljon <faxriddinov0620@gmail.com>
2024-06-07 22:34:04 +0500

    v1

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,41 @@
 import { createWebHistory, createRouter } from "vue-router";
+import Home from "../components/Home/Home.vue";
+import AboutUs from "../components/AboutUs/AboutUs.vue";
+import Pricing from "../components/Pricing/Pricing.vue";
+import Work from "../components/Work/Work.vue";
+import Blog from "../components/Blog/Blog.vue";
+import Contact from "../components/Contact/Contact.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import("../components/Home/Home.vue"),
+    component: Home,
   },
   {
     path: "/about",
     name: "About",
-    component: import("../components/AboutUs/AboutUs.vue"),
+    component: AboutUs,
   },
   {
     path: "/pricing",
     name: "Pricing",
-    component: import("../components/Pricing/Pricing.vue"),
+    component: Pricing,
   },
-
   {
     path: "/work",
     name: "Work",
-    component: import("../components/Work/Work.vue"),
+    component: Work,
   },
   {
     path: "/blog",
     name: "Blog",
-    component: import("../components/Blog/Blog.vue"),
+    component: Blog,
   },
   {
     path: "/contact",
     name: "Contact",
-    component: import("../components/Contact/Contact.vue"),
+    component: Contact,
   },
 ];
 
